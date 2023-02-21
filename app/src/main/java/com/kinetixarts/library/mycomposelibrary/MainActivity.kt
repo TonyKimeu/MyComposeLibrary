5534f1ebbd18bd3b2c1da83dc3dffc2d01c4f0a3
AnthonyMwanzia <anthony.mwanzia@mgas.ke>
2023-02-21 21:29:44 +0300

    Use library

--- app/src/main/java/com/kinetixarts/library/mycomposelibrary/MainActivity.kt
+++ app/src/main/java/com/kinetixarts/library/mycomposelibrary/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kinetixarts.library.image_preview.ImagePreview
 import com.kinetixarts.library.mycomposelibrary.ui.theme.MyComposeLibraryTheme
 
@@ -20,11 +24,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyComposeLibraryTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Box( modifier = Modifier.fillMaxSize()) {
+                    ImagePreview(
+                        image = painterResource(id = R.drawable.kermit),
+                        description = "Hello World",
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(150.dp)
+                    )
+
                 }
 
             }
